0e8e49b789484308df0ac6139494dad2ab767633
KimSongEun <songeun4939@naver.com>
2021-06-16 15:26:42 +0900

    Create linear.ino
    linear

---
+++ linear/linear.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+#define LINEAR 9        //Linear Actuator
+
+#define L16_MSMIN      1000
+#define L16_MSMAX      2000
+
+Servo L16;
+
+int L16value = 10;
+
+int button = 0;
+int BMAX = 0;
+int BMIN = 0;
+
+void setup()
+{ Serial.begin(9600);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+
+  L16.attach(LINEAR, L16_MS, L16_MS2);
+  L16.writeMicroseconds(L16value);
+}
+
+void loop() {
+  BMAX = digitalRead(input);
+
+  if (BMAX == HIGH) {
+    while (button != 100) {
+      button++;
+
+      L16Value = map(button, 0, 100, L16_MS, L16_MS2);
+      L16.writeMicroseconds(L16Value);
+    }
+  }
+
+  BMIN = digitalRead(input2);
+  if (BMIN == HIGH) {
+    while (button != 0) {
+      button--;
+
+      L16Value = map(button, 0, 100, L16_MS, L16_MS2);
+      L16.writeMicroseconds(L16Value);
+    }
+  }
+
+}
