dd784a4bad5b50b0908c424dd63b31200db20a47
ansu7514 <54002821+ansu7514@users.noreply.github.com>
2021-06-16 15:10:41 +0900

    Update Grove-Water_Atomization.ino

--- Grove-Water_Atomization/Grove-Water_Atomization.ino
+++ Grove-Water_Atomization/Grove-Water_Atomization.ino
@@ -1,20 +1,18 @@
 void setup() {
   Serial.begin(9600);
-  pinMode(13, INPUT);
   
-  // put your setup code here, to run once:
+  pinMode(13, INPUT);
   pinMode(A5, OUTPUT);
 }
 
 void loop() {
   int buttonValue = digitalRead(13);
-  
-  
-  if(buttonValue == HIGH) {
+
+  if (buttonValue == HIGH) {
     Serial.println("Swich ON");
     digitalWrite(A5, HIGH);
   }
-  else if(buttonValue == LOW) {
+  else if (buttonValue == LOW) {
     Serial.println("Swich OFF");
     digitalWrite(A5, LOW);
   }
